ac2ec46d48ce3fa0540c0319512731fe16e8bc5b
Taylor Sabbag <taylorsabbag@gmail.com>
2024-12-12 19:29:18 -0500

    refactor: simplify solutions export.

---
+++ src/lib/solutions/2024/index.ts
@@ -0,0 +1,3 @@
+export { default as Day1 } from "./Day1.svelte";
+export { default as Day2 } from "./Day2.svelte";
+
--- src/lib/solutions/index.ts
+++ src/lib/solutions/index.ts
@@ -1,16 +1,18 @@
-import Day1 from "./2024/Day1.svelte";
+import * as Day2024 from "./2024";
 import NotFoundSolution from "./NotFoundSolution.svelte";
 
 type SolutionProps = { year: string; day: number };
-type Solution = typeof Day1 & { props: SolutionProps };
+type Solution = typeof Day2024.Day1 & { props: SolutionProps };
 
 export const solutions: Record<string, Record<number, Solution>> = {
-    "2024": {
-        1: Day1
-    }
+	"2024": Object.fromEntries(
+		Array.from({ length: 24 }, (_, i) => [
+			i + 1,
+			Day2024[`Day${i + 1}` as keyof typeof Day2024],
+		]),
+	),
 };
 
 export const getSolution = (year: string, day: number): Solution => {
-    return (solutions[year]?.[day] ?? NotFoundSolution) as Solution;
+	return (solutions[year]?.[day] ?? NotFoundSolution) as Solution;
 };
-
